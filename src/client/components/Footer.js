8ceea58919bea2e1ad56bc705c328c8e08d6de55
김상훈 <kimsh0117@kimsh0117-MacBook-Pro.local>
2018-02-10 16:22:39 +0900

    2월10일4시22분까지작업

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -8,16 +8,18 @@ const Footer = () => {
   return (
     <Container className="footer">
       <Row>
-          <Col xs="6">
+          <Col>
             <Link to="/terms">이용약관</Link><br />
             <Link to="/about">About</Link><br />
             <Link to="/privacy">개인정보처리방침</Link>
           </Col>
-          <Col xs="6">
+          <Col>
             고객센터<br />
             010-0000-0000<br />
             info@goodparents.org
           </Col>
+          <Col></Col>
+          <Col></Col>
       </Row>
     </Container>
   )
