6c9d7699d8dca60c383a87ecc07065bcee64046e
Joe1220 <joe1220@daum.net>
2018-02-10 21:44:41 +0900

    about page done

--- src/client/components/Area.js
+++ src/client/components/Area.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Jumbotron } from 'reactstrap';
+import { Jumbotron, Button } from 'reactstrap';
 import "../public/style/Area.css";
 
 export const Area = (props) => {
   return (
     <div>
       <Jumbotron>
-        <div className="mx-5" style={{width: 25 + 'em'}}>
-          <h3 className="display-5 font-weight-bold">신선한 재료를<br />문 앞에서 만나세요</h3>
-          <p className="lead">최대한 신선한 상품을 전하고자<br />
-          주문한 다음 날 도착하는 배송을 시작했습니다.<br />
-          클릭 한 번으로 기분 좋은 하루를 선물받으세요.</p>
-        </div>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
       </Jumbotron>
     </div>
   );
