f56f55892eab743d97ad86e00b80d3471f6776c1
김상훈 <kimsh0117@kimsh0117-MacBook-Pro.local>
2018-02-12 19:49:47 +0900

    foodDetail

--- src/client/components/FoodDetail.js
+++ src/client/components/FoodDetail.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 import '../public/style/FoodDetail.css'
+import image from '../public/img/food_detail_2.jpg'
+
 
 class FoodDetail extends Component {
   render(){
@@ -9,8 +11,26 @@ class FoodDetail extends Component {
       <Container className="foodDetailContainer">
         <div className="foodDetailInnerContainer">
           <Row>
-            <Col>1</Col>
-            <Col>2</Col>
+            <Col>
+              <img className="foodDetailimg" src={image} alt="food"/>
+            </Col>
+            <Col>
+              <div className="foodDetailDate">
+                오늘<br />
+                3월 12일 수요일
+              </div>
+              <div className="foodDetailTime">
+              아침
+              <hr align="left"/>
+              </div>
+              <div className="foodDetailItem">
+                클래식 한우버섯
+                <div className="foodDetailItemPrice">4,500</div>
+                <Button color="primary">장바구니에 담기</Button>
+              </div>
+              <div>
+              </div>
+            </Col>
           </Row>
         </div>
       </Container>
