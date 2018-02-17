79ae33444d93f9e231cac27f413b8a2a7858cc63
Joe1220 <joe1220@daum.net>
2018-02-17 17:06:15 +0900

    home diet food item continue...

--- src/client/components/HomeDiet.js
+++ src/client/components/HomeDiet.js
@@ -3,12 +3,23 @@ import React from 'react';
 import HomeDietDate from './HomeDietDate'
 import HomeDietFood from './HomeDietFood'
 
-const HomeDiet = () => {
+// const HomeDiet = () => {
+//   return(
+//     <div className="HomeDiet">
+//       <div className="HomeDietContainer">
+//         <HomeDietDate />
+//         <HomeDietFood />
+//       </div>
+//     </div>
+//   )
+// };
+
+const HomeDiet = ({products}) => {
   return(
     <div className="HomeDiet">
       <div className="HomeDietContainer">
         <HomeDietDate />
-        <HomeDietFood />  
+        <HomeDietFood products={products}/>
       </div>
     </div>
   )
