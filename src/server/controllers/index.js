f94fec6e81650d863ee72683b66c1d7d591cb5b1
Jo Jinsik <kunggom@gmail.com>
2018-02-21 15:54:12 +0900

    foodDetail API 생성 및 변경.

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -3,7 +3,7 @@ var models = require("../models");
 module.exports = {
   foodDetail: {
     get: function(req, res) {
-      models.food.get(function(error, results) {
+      models.food.get(req.query.date, function(error, results) {
         if (error) console.error(error);
         res.json(results);
       });
---
+++ src/server/models/pickItem.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const mockData = __dirname + "/MOCK_DATA.json";
+
+const randomGen = function(range) {
+  return Math.floor(Math.random() * range);
+};
+
+const readData = function(filePath) {
+  try {
+    const contents = fs.readFileSync(filePath);
+    return JSON.parse(contents);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// const randomPick = function() {
+//   const list = readData(mockData);
+//   const idx = randomGen(list.length);
+//   return list[idx];
+// };
+
+// const randomPick3 = function() {
+//   const result = [];
+//   for (let i = 0; i < 3; i++) {
+//     result.push(randomPick());
+//   }
+//   return result;
+// };
+
+const pickByDate = function(date) {
+  date = new Date(date).toISOString();
+  const list = readData(mockData);
+  for (let i of list) {
+    if (i.date === date) return i.foods;
+  }
+  return null;
+};
+
+module.exports = pickByDate;
+// console.log(pickByDate("2018-02-23"));
