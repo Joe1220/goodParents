271de87b24c18266997a36a8a7de48db225b1492
김상훈 <kimsh0117@kimsh0117-MacBook-Pro.local>
2018-02-27 10:37:17 +0900

    date debugged

--- src/server/models/products.js
+++ src/server/models/products.js
@@ -1,8 +1,8 @@
 const pickItem = require("./pickItem");
 
 module.exports = {
-  get: function(date, callback) {
-    const today = new Date().toISOString().slice(0, 10);
-    callback(null, pickItem(today));
+  get: function(req, callback) {
+    const date = req.query.date;
+    callback(null, pickItem(date));
   }
 };
