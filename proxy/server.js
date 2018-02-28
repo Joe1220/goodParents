2cc3d8047ba3f10e7a534054f6f7668dc03c68d5
Jo Jinsik <kunggom@gmail.com>
2018-02-28 21:31:54 +0900

    도커 코드 일부 개선.

--- proxy/server.js
+++ proxy/server.js
@@ -8,10 +8,10 @@ const apiPort = process.env.APIPORT || 3001;
 const app = express();
 
 app.use("/", proxy({
-  target: `http://goodparents_front_1:${frontPort}`,
+  target: `http://front:${frontPort}`,
   changeOrigin: true,
   router: {
-    "/api": `http://goodparents_back_1:${apiPort}`
+    "/api": `http://back:${apiPort}`
   }
 }));
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const helmet = require('helmet');
+const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 3001;
+const mongoPort = process.env.MONGOPORT || 27017;
 
 const UsersRouter = require("./routes/users");
 const ProductsRouter = require("./routes/products");
@@ -17,17 +18,18 @@ app.use(bodyParser.json());
 // Helmet을 이용하면 HTTP 헤더를 적절히 설정하여 몇 가지 잘 알려진 웹 취약성으로부터 앱을 보호할 수 있다.
 app.use(helmet());
 // X-Powered-By 헤더는 사용하지 않도록 설정
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 // 쿠키파서 미들웨어 연결
 app.use(cookieParser());
 // 몽고디비 커넥션
 mongoose
-  .connect("mongodb://goodparents_mongodb_1:27017/goodParents")
+  .connect(`mongodb://mongodb:${mongoPort}/goodParents`)
   .then(() => {
-    console.log("connected mongodb");
+    console.log("mongodb connected.");
   })
   .catch(() => {
-    console.error("DB connect error");
+    console.error("DB connect error. Exit.");
+    process.exit(1);
   });
 
 // 라우팅 연결
