d1ed41d7979750d7b26330c1556ce13c482e73fb
joe1220 <joe1220@daum.net>
2018-03-05 11:08:36 +0900

    pay check page done

--- src/client/components/myPage/MyPagePayCheck.js
+++ src/client/components/myPage/MyPagePayCheck.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../../public/style/MyPagePayCheck.css';
+import { Link } from 'react-router-dom';
 
 class MyPagePayCheck extends React.Component {
   render() {
     return (
-      <div>MyPagePayCheck</div>
+      <div className="mypage_paycheck_main">
+        <p className="mypage_paycheck_main_upper_Home_text"><Link to="/mypage/ordercheck">Home</Link> / 결제정보관리</p>
+        <div className="mypage_paycheck_main_description_box">
+          <p id="mypage_paycheck_main_head">신용카드 정보 확인</p>
+          <hr id="mypage_paycheck_main_hr"/>
+          <p className="mypage_paycheck_main_card_text">신용카드</p>
+          <p id="mypage_paycheck_main_card_text_num" className="mypage_paycheck_main_card_text">**** **** **** 4578</p>
+        </div>
+      </div>
     );
   }
 }
