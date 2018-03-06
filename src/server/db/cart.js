ea2d88b3c86200822109760d35d5f5f1a4b82851
김상훈 <kimsh0117@kimsh0117-MacBook-Pro.local>
2018-03-06 13:48:30 +0900

    add mongoose schema

--- src/server/db/cart.js
+++ src/server/db/cart.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const CartSchema = new mongoose.Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
@@ -8,4 +9,4 @@ const CartSchema = new mongoose.Schema({
   ]
 });
 
-module.exports = mongoose.model("Cart", CartSchema);
+module.exports = mongoose.model('Cart', CartSchema);
