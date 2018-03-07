d747347af3f917d274ea47ebba0c16abb3215966
Jo Jinsik <kunggom@gmail.com>
2018-03-07 18:10:44 +0900

    counseling api 완성.

--- src/server/routes/mypage.js
+++ src/server/routes/mypage.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const BabyController = require("../controllers/baby");
 const BillingController = require("../controllers/billing");
+const CounselingController = require("../controllers/counseling");
 const router = express.Router();
 
 router.get("/baby", BabyController.get);
@@ -10,5 +11,8 @@ router.delete("/baby", BabyController.delete);
 
 router.get("/billing", BillingController.get);
 router.put("/billing", BillingController.put);
+
+router.get("/counseling", CounselingController.get);
+router.post("/counseling", CounselingController.post);
 
 module.exports = router;
