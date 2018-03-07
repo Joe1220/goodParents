8d31641cf4ed135cc42032134287a7c32863c32c
Jo Jinsik <kunggom@gmail.com>
2018-03-07 10:46:30 +0900

    billing api 완료.

--- src/server/routes/mypage.js
+++ src/server/routes/mypage.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const BabyController = require("../controllers/baby");
+const BillingController = require("../controllers/billing");
 const router = express.Router();
 
 router.get("/baby", BabyController.get);
 router.post("/baby", BabyController.post);
 router.put("/baby", BabyController.put);
 router.delete("/baby", BabyController.delete);
+
+router.get("/billing", BillingController.get);
+router.put("/billing", BillingController.put);
 
 module.exports = router;
