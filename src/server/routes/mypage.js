45e29176d002d0159a26986c0af2d76fe282c63e
Jo Jinsik <kunggom@gmail.com>
2018-03-08 22:09:19 +0900

    order API 완성.

--- src/server/routes/mypage.js
+++ src/server/routes/mypage.js
@@ -4,6 +4,7 @@ const BillingController = require("../controllers/billing");
 const CounselingController = require("../controllers/counseling");
 const AccountController = require("../controllers/account");
 const DeliveryController = require("../controllers/delivery");
+const OrderController = require("../controllers/order");
 const ExchangeController = require("../controllers/exchange");
 
 const router = express.Router();
@@ -26,6 +27,10 @@ router.get("/delivery", DeliveryController.get);
 router.post("/delivery", DeliveryController.post);
 router.put("/delivery", DeliveryController.put);
 router.delete("/delivery", DeliveryController.delete);
+
+router.get("/order", OrderController.get);
+router.put("/order", OrderController.put);
+router.delete("/order", OrderController.delete);
 
 router.get("/exchange", ExchangeController.get);
 router.post("/exchange", ExchangeController.post);
