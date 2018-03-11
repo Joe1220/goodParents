e21f1eb35c2a67d617d3af1b664e3418d3feb526
김상훈 <kimsh0117@kimsh0117-MacBook-Pro.local>
2018-03-11 17:25:11 +0900

    complete orderhistory design

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 
-
 import About from "./About";
 import Area from "./Area";
 import Home from "./Home";
@@ -26,6 +25,7 @@ class App extends Component {
       cart: [],
       fullDate: new Date().toISOString().slice(0, 10),
       account: [],
+      orderhistory: [],
       snackbar: {
         account: false,
         fooddetail: false
@@ -45,6 +45,7 @@ class App extends Component {
     // 스넥바 메소드
     this.snackbarOpen = this.snackbarOpen.bind(this);
     this.snackbarClose = this.snackbarClose.bind(this);
+    // 주문확인 메소드
   }
 
   foodDetailFetch() {
@@ -52,6 +53,16 @@ class App extends Component {
       .then(response => response.json())
       .then(data => this.setState({ products: data[0].items }))
       .catch(error => console.error(error));
+  }
+  // 주문확인
+  getOrderHistory() {
+    const url = "/api/mypage/order"
+    fetch(url,{
+      credentials: "include"
+    })
+    .then(response => response.json())
+    .then(data => this.setState({ orderhistory: data || [] }))
+    .catch(error => console.error(error));
   }
   // 장바구니 관련 메소드
   getCart() {
@@ -199,6 +210,7 @@ class App extends Component {
     this.foodDetailFetch();
     this.getCart();
     this.getAccount();
+    this.getOrderHistory();
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -310,7 +322,8 @@ class App extends Component {
               account={this.state.account}
               changeAccount={this.changeAccount}
               snackbar={this.state.snackbar.account}
-              snackbarClose={this.snackbarClose} />;
+              snackbarClose={this.snackbarClose}
+              orderhistory={this.state.orderhistory} />;
             }}
           />
           {/* AdminPage */}
--- src/client/components/mypage/Check.js
+++ src/client/components/mypage/Check.js
@@ -1,8 +1,69 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 
+const styles = {
+  paper: { color: "gray", marginBottom: "15px" },
+  when: { fontSize: "17px", fontWeight: "normal", padding: "15px" },
+  hr: { marginTop: "0"},
+  orderbox: { padding: "0 15px 0 15px", display: "flex" }
+}
 
 export default class Check extends Component{
   render(){
-    return <h1>Check</h1>
+    console.log("check",this.props.orderhistory)
+    return (
+      <MuiThemeProvider>
+        <Paper style={styles.paper}>
+          <div style={styles.when}>2018년 3월 18일</div>
+          <hr style={styles.hr}/>
+          <div style={styles.orderbox}>
+            <div style={{ width: "30px", margin: "15px"}}>1</div>
+            <div style={{ width: "100px", margin: "15px"}}>3월 15일</div>
+            <div style={{ width: "250px", margin: "15px"}}>시금치무국</div>
+            <div style={{ width: "70px", margin: "15px"}}>2개</div>
+            <div style={{ width: "100px", margin: "15px"}}>배송중</div>
+            <div style={{ width: "100px", margin: "15px", marginTop: "6px"}}>
+              <RaisedButton label="취소" secondary={true}  />
+            </div>
+          </div>
+          <div style={styles.orderbox}>
+            <div style={{ width: "30px", margin: "15px"}}>1</div>
+            <div style={{ width: "100px", margin: "15px"}}>3월 15일</div>
+            <div style={{ width: "250px", margin: "15px"}}>시금치무국</div>
+            <div style={{ width: "70px", margin: "15px"}}>2개</div>
+            <div style={{ width: "100px", margin: "15px"}}>배송중</div>
+            <div style={{ width: "100px", margin: "15px", marginTop: "6px"}}>
+              <RaisedButton label="취소" secondary={true}  />
+            </div>
+          </div>
+        </Paper>
+        <Paper style={styles.paper}>
+          <div style={styles.when}>2018년 3월 18일</div>
+          <hr style={styles.hr}/>
+          <div style={styles.orderbox}>
+            <div style={{ width: "30px", margin: "15px"}}>1</div>
+            <div style={{ width: "100px", margin: "15px"}}>3월 15일</div>
+            <div style={{ width: "250px", margin: "15px"}}>시금치무국</div>
+            <div style={{ width: "70px", margin: "15px"}}>2개</div>
+            <div style={{ width: "100px", margin: "15px"}}>배송중</div>
+            <div style={{ width: "100px", margin: "15px", marginTop: "6px"}}>
+              <RaisedButton label="취소" secondary={true}  />
+            </div>
+          </div>
+          <div style={styles.orderbox}>
+            <div style={{ width: "30px", margin: "15px"}}>1</div>
+            <div style={{ width: "100px", margin: "15px"}}>3월 15일</div>
+            <div style={{ width: "250px", margin: "15px"}}>시금치무국</div>
+            <div style={{ width: "70px", margin: "15px"}}>2개</div>
+            <div style={{ width: "100px", margin: "15px"}}>배송중</div>
+            <div style={{ width: "100px", margin: "15px", marginTop: "6px"}}>
+              <RaisedButton label="취소" secondary={true}  />
+            </div>
+          </div>
+        </Paper>
+      </MuiThemeProvider>
+    )
   }
 }
