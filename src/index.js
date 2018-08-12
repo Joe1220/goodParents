50c9b40faba0f45adfb637db77d465a1d17dbe30
joe1220 <joe1220@daum.net>
2018-08-12 22:26:58 +0900

    delete registerWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import Root from './client/Root';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
