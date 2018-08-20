4fdd4a5fe8885f56ca8d95b23402ae71ec92be53
joe1220 <joe1220@daum.net>
2018-08-20 14:38:56 +0900

    comment result in controller console

--- src/server/controllers/products.js
+++ src/server/controllers/products.js
@@ -4,7 +4,7 @@ module.exports = {
   get: (req, res) => {
     ProductsModels.get(req, (error, results) => {
       if(error) res.status(500).send(error);
-      console.log('result in controller', results)
+      // console.log('result in controller', results)
       res.json(results);
     });
   }
