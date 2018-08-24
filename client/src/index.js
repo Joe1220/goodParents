4f075d460f53374314c8555c4c07db04ed372138
joe1220 <joe1220@daum.net>
2018-08-24 18:55:59 +0900

    fix re structure errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { BrowserRouter } from "react-router-dom";
-import Root from './client/Root';
+import Root from './Root';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
